b256a4e6bcb2053251746d785f93dc66ba201839
Joseph Russell <joseph@wearejh.com>
2014-09-30 11:26:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js ie ie6 lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en-US"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js ie ie7 lt-ie10 lt-ie9 lt-ie8" lang="en-US"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en-US"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js ie ie9 lt-ie10" lang="en-US"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js ffly" lang="en-US"> <!--<![endif]-->
+<head>
+    <title>Nott Tuesday: Meetup for Founders and Entrepreneurs in Nottingham</title>
+    <meta name="description" content="Presentations and conversations about starting and growing a business. Share, support and unwind every third Tuesday of the month.">
+
+    <!-- META -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ICONS -->
+    <link href="favicon.ico" rel="icon" type="image/ico">
+
+    <!-- CSS -->
+    <script type="text/javascript" src="//use.typekit.net/gpq2kyz.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Roboto:400,700|Roboto+Slab:400,700' rel='stylesheet' type='text/css'> -->
+    <link rel="stylesheet" href="nt.css">
+    <?php /*
+    <script src="assets/js/dist/modernizr.custom.min.js"></script>
+    <link rel="stylesheet" href="assets/css/core.min.css">
+    <!--[if (lt IE 9) & (!IEMobile)]>
+    <link rel="stylesheet" href="assets/css/ie.min.css">
+    <![endif]-->
+    */ ?>
+</head>
+<body>
+
+<div class="wrapper fill--green">
+    <div class="container">
+
+        <header class="site-header" role="banner">
+            <a href="" class="logo"><img src="http://placehold.it/100x100/ffffff" alt=""></a>
+        </header><!-- end .site-header -->
+
+        <div class="intro">
+            <!--
+            meetup
+            social networking event
+            business networking event
+            technology industry networking forum
+
+            share, support and unwind
+
+            tech industry
+            tech businesses
+            tech community
+            technology people
+            startup scene
+            founders and entrepreneurs
+             -->
+            <h1>A meetup for Nottingham&rsquo;s tech scene.</h1>
+            <p>Our free, monthly event features a guest speaker and time to meet fellow founders, entrepreneurs and technologists. Come share, support and unwind with us.</p>
+        </div>
+
+    </div>
+</div>
+
+<div class="wrapper">
+    <div class="container">
+
+        <div class="next">
+            <div class="summary">
+                <h2>Next Event</h2>
+                <ul class="details">
+                    <li class="date">16th September</li>
+                    <li class="time">6:30 &ndash; 8:30pm</li>
+                    <li class="venue"><a href="https://www.google.co.uk/maps/place/Antenna,+9A+Beck+St,+Nottingham+NG1+1EQ/@52.955938,-1.143181,17z/data=!4m2!3m1!1s0x4879c17de0dc5f45:0x4d78304762fccbec">Antenna</a></li>
+                    <li class="price">Free</li>
+                </ul>
+                <p class="rsvp"><a class="button" href="">RSVP on Meetup</a></p>
+            </div>
+            <div class="speaker">
+                <h3>Business and Numbers</h3>
+                <!-- <h3><strong>Business and Numbers</strong> with <a href="">Jane Smith</a></h3> -->
+                <img src="face6.jpg" alt="Jane Smith">
+                <p><a href="">Jane Smith</a> is co-founder of Acme Inc. and lectures at Nottingham University. She will be talking about how business and numbers are the foundation of every successful company. Jane will look at how to apply these build a new business around these keystones.</p>
+            </div>
+        </div>
+
+        <div class="sponsors">
+            <h2>Sponsors</h2>
+            <ul>
+                <li>
+                    <a href=""><img src="paypal.png" alt="PayPal"></a>
+                    <a href="">PayPal</a> makes it easy for businesses to get paid. Whether you&rsquo;re online or offline, in your shop or on the road, one simple PayPal account is all you need.
+                </li>
+                <li>
+                    <a href=""><img src="mailchimp.png" alt="MailChimp"></a>
+                    <a href="">MailChimp</a> is used by more than 6 million people to design and send beautiful email marketing campaigns, then track results to see how their business is affected.
+                </li>
+                <li>
+                    <a href=""><img src="github.png" alt="GitHub"></a>
+                    <a href="">GitHub</a> is the best place to share code with friends, co-workers, and complete strangers. Over five million people use GitHub to build amazing things together.
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+
+<div class="wrapper fill--black">
+    <div class="container">
+
+        <div class="about">
+            <h2>About Nott Tuesday</h2>
+            <p># REWRITE #. Each month we have stimulating speakers giving talks on the challenges of starting and running a technology business, practical guidance on operating a business such as finding funding and exploring future technologies for new opportunities.</p>
+            <p>The ethos of our group is bring together technology people, startups and companies. Our vision is to fill the Nottingham tech scene with networking and opportunities.</p>
+            <p>Nott Tuesday is brought to you by <a href="https://twitter.com/nickpoint">Nick Barker</a> and <a href="https://twitter.com/jhuskisson">Jamie Huskisson</a>.</p>
+            <h3>Want to speak at Nott Tuesday?</h3>
+            <p>If you&rsquo;d like to propose a talk or make a recommendation for a speaker, please contact us through <a href="mailto:hello@notttuesday.com ">hello@notttuesday.com</a> or <a href="http://twitter.com/notttuesday">@notttuesday</a>.</p>
+        </div>
+
+
+        <div class="past">
+            <h2>Previous Events</h2>
+            <ul>
+                <li>
+                    <img src="face1.jpg" alt="">
+                    <div>
+                        <time datetime="2014-08">August 2014</time>
+                        <h3><strong>Leadership</strong> with <a href="">Adam Harris</a></h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="face2.jpg" alt="">
+                    <div>
+                        <time datetime="2014-07">July 2014</time>
+                        <h3><strong>User Needs</strong> with <a href="">Christine Cawthorne</a></h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="face3.jpg" alt="">
+                    <div>
+                        <time datetime="2014-06">June 2014</time>
+                        <h3><strong>Space Technology</strong> with <a href="">Doug Watson</a>, <a href="">Mark Hapgood</a> and <a href="">Stephen Fuller</a></h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="face4.jpg" alt="">
+                    <div>
+                        <time datetime="2014-05">May 2014</time>
+                        <h3><strong>The Greed Gene</strong> with <a href="">Aamir Butt</a></h3>
+                    </div>
+                </li>
+                <li>
+                    <img src="face5.jpg" alt="">
+                    <div>
+                        <time datetime="2014-04">April 2014</time>
+                        <h3><strong>Lean Thinking</strong> with <a href="">Toby Reid</a></h3>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+
+    </div>
+</div>
+
+<!-- <div class="wrapper">
+    <div class="container">
+
+
+    </div>
+</div> -->
+
+<footer class="site-footer" role="contentinfo">
+
+    <!-- <div class="social">
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </div> -->
+
+    <!-- <div class="legal">
+        <p>&copy;<?php echo date('Y'); ?> Nott Tuesday.</p>
+    </div> -->
+
+</footer><!-- end .site-footer -->
+
+<!-- JS -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bower_components/requirejs/require.js" data-main="<?php echo get_template_directory_uri(); ?>/assets/js/config"></script>
+<!-- <script src="<?php echo get_template_directory_uri(); ?>/assets/js/dist/modules.combined.min.js"></script> -->
+
+</body>
+</html>
+
